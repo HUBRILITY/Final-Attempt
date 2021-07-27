cfa1c66c8f37590e13fbe9011a6be6a34f17134d
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-26 20:50:31 -0400

    Task: Updated app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -27,7 +28,14 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
